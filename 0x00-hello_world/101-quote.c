ed1bb05c59598a2cb115cf10c8f3e31857e0aa33
zye7ert <edgarxot@gmail.com>
2021-06-22 04:55:56 +0000

    9. UNIX is basically a simple operating system, b

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - main function
+ *
+ * return : print a message with printf
+ *
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
