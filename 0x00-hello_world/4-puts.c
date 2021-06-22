78446fded9c260c4dfdfd4b71c27f978c1717690
zye7ert <edgarxot@gmail.com>
2021-06-22 02:50:51 +0000

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+nclude<stdio.h>
+
+/**
+* main - Main function
+*
+* Return: Print a message with puts
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
