5ed071e225ec7cf6562982bc61dad46ce7e3d847
zye7ert <edgarxot@gmail.com>
2021-06-22 04:39:39 +0000

    9. UNIX is basically a simple operating system, b

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,24 +2,13 @@
 #include <string.h>
 
 /**
-* Write a script that generates the assembly code (Intel syntax) of a C code and save it in an output file.
+*main - Main function
 *
-* The C file name will be saved in the variable $CFILE.
-*The output file should be named the same as the C file, but with the extension .s instead of .c.
-*Example: if the C file is main.c, the output file should be main.s
-* 
+* Return: Print a message with printf
 */
 
 int main(void)
 {
-	char l[99] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int length = strlen(l);
-	int i = 0;
-	
-	while (i < length)
-	{
-		putchar(l[i]);
-		i++;
-	}
-	return (1);
+	printf ( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
 }
