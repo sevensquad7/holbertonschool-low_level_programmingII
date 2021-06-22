817b12efab62b686a4a4a7c94b4f9696edf24561
zye7ert <edgarxot@gmail.com>
2021-06-22 04:04:39 +0000

    9. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Main function
+*
+* Return: Print a message without printf or puts
+*/
+int main(void)
+{
+	char t[70] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int longitud = strlen(t);
+	int i = 0;
+	while (i < longitud)
+{
+	putchar(t[i]);
+	i++;
+}
+	return (1)
+}
