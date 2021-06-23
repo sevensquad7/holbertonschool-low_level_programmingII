df3dac19d8813019d324884f2a00e3aadbaeffbd
zye7ert <edgarxot@gmail.com>
2021-06-23 14:40:39 +0000

    0. _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
+
 /*
 * Main : main function
 *
 * Return: Print a message \"Holberton\"
 */
+
 int main(void)
 {
-	char message = "Holberton";
+	char message[] = "Holberton";
 	int i = 0;
-	int cadena=0;
-	cadena = sizeof(message);
-	
-	for (i=0;i<=cadena;i++)
+	int cadena=sizeof(message);
+
+	for (i=0;i<cadena;i++)
 	{
 		_putchar(message[i]);
 	}
