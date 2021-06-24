7678506aaf941da7ecdab20850af0e064893067d
zye7ert <edgarxot@gmail.com>
2021-06-24 18:11:08 +0000

    11. Holberton School, 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,19 +10,20 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		if (n > 98){
-		printf("%d", n);
-		printf(",");
-		printf(" ");
-		n--;
-	} 
-	else
-	{
-		printf("%d", n);
-		printf(",");
-		printf(" ");
-		n++;
-	} 
+		if (n > 98)
+		{
+			printf("%d", n);
+			printf(",");
+			printf(" ");
+			n--;
+		}
+		else
+		{
+			printf("%d", n);
+			printf(",");
+			printf(" ");
+			n++;
+		}
 	}
 	printf("%d\n", n);
 }
