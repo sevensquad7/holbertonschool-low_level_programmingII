e02b5ef66b73d0a6a39fec7be2848f2247e4460d
zye7ert <edgarxot@gmail.com>
2021-06-24 00:48:28 +0000

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Alphabet
- */
+* main - check the code for Holberton School students.
+*
+* Return: Alphabet
+*/
 void print_alphabet(void)
 {
 	char letra;
