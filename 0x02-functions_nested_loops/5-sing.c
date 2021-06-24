0313b72584160b71a5541e5bc2bb65b17e29fc9f
zye7ert <edgarxot@gmail.com>
2021-06-24 04:30:28 +0000

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sing.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_sign - function that prints the sign of a number
+ *@n: the number
+ * Return: 1 and prints + if n is greater than zero
+ * Return: 0 and prints 0 if n is zero
+ * Return: -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
