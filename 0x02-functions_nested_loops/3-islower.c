51cab7288a1fba59c6a12025145a2a7cd4812c5e
zye7ert <edgarxot@gmail.com>
2021-06-24 01:51:51 +0000

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/*
+ * _islower - lower is 1 and other 0
+ *
+ * return: 1 is lowercase and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if(islower(c))
+	{	
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
