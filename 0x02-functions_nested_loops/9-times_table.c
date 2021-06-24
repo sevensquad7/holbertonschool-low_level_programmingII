c2d33aaa640d0851273e334f9191837df7e8f0f0
zye7ert <edgarxot@gmail.com>
2021-06-24 15:29:35 +0000

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * times_tables - this function sorts the data in a table
+ *
+ * Returns: 9x9 multiplication
+ *
+ */
+void times_table(void)
+{
+	int i=0,j=0,r=0;
+	int unidades=0;
+	int decenas=0;
+
+	for (i=0; i <= 9; i++)
+	{
+		for (j =0; j <= 9; j++)
+		{
+			r = (i * j);
+			unidades = r%10;
+			decenas = r/10;
+			if (decenas==0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + unidades);
+				if (j < 9)
+				{
+					_putchar(',');
+				}
+				else
+				{
+					_putchar('$');
+				}
+			} 
+			else 
+			{
+				_putchar(' ');
+				_putchar('0' + decenas);
+				_putchar('0' +  unidades);
+				if (j < 9)
+				{
+					_putchar(',');
+				}
+				else
+				{
+				_putchar('$');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
