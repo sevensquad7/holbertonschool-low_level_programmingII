1464b085f1f15b18cf5f5615cd63a836544776d9
zye7ert <edgarxot@gmail.com>
2021-06-24 01:16:30 +0000

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,13 @@
 /**
  * print_alphabet_x10 - print alphabet
  *
- * return: alphabet x10 
+ * return: alphabet x10
  */
 void print_alphabet_x10(void)
 {
 	char letra;
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 	for (letra = 'a' ; letra <= 'z' ; letra++)
