04390854a49a34dab92de46fcf0f52a9a2193a42
zye7ert <edgarxot@gmail.com>
2021-06-27 03:31:48 +0000

    style correction 3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,11 @@
 
 /*
  * mul - function that prints the numbers, from 0 to 9, followed by a new line.
- * 
+ *
  * Return: axb result
  */
 
- void print_numbers(void)
+void print_numbers(void)
 {
 	int a;
 
