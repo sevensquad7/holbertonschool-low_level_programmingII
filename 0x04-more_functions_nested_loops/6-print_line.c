e68df414b02550b224c6b85330b08143a991f003
zye7ert <edgarxot@gmail.com>
2021-06-28 04:46:30 +0000

    6. The shortest distance between two points is a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,7 +5,7 @@
  * You can only use _putchar function to print
  * @n: is the number of times the character
  * Return: n is the number of times the character _ should be printed
- * 
+ *
  */
 
 void print_line(int n)
@@ -14,7 +14,7 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for( i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
