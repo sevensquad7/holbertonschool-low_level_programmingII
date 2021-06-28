d3fdd9bcf27ea0751d422ae72d065e7b59c986a4
zye7ert <edgarxot@gmail.com>
2021-06-28 05:22:59 +0000

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,7 +4,7 @@
  * print_triangle - function that prints a triangle
  * followed by a new line.
  * You can only use _putchar function to print
- * Where size is the size of the triangle
+ * @size: is the size of the triangle
  * Return: If size is 0 or less, the function should print only a new line
  * Use the character # to print the triangle
  */
