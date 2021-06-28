621703f94e3d4866c19fd34f5894bdb4a25c0b56
zye7ert <edgarxot@gmail.com>
2021-06-28 05:13:56 +0000

    8. You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * print_diagonal - function that draws a diagonal line on the terminal.
+ * print_square - function that prints a square, followed by a new line.n
  * You can only use _putchar function to print
- * size is the size of the square
+ * @size: fimentons of the square
  * Return: If size is 0 or less, the function should print only a new line
  * Use the character # to print the square
  */
 
 void print_square(int size)
 {
-	int i, j;
+	int i = 0, j = 0;
 
 	if (size <= 0)
 		_putchar('\n');
