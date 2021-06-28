ca5567f1d431b3b1d8dbbb735fa85d6d43d7176a
zye7ert <edgarxot@gmail.com>
2021-06-28 04:20:51 +0000

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_numbers - function that prints the numbers, from 0 to 9, followed by a new line.
+ * print_numbers - function that prints the numbers, from 0 to 9
+ * followed by a new line.
  * you can only use _putchar twice in your code
  * Return: return axb result
  */
