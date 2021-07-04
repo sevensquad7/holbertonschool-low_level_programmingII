6b912af37d908befacef35ee58b689176a0a83f8
zye7ert <edgarxot@gmail.com>
2021-07-04 22:11:42 +0000

    12. Numbers have life; they're not just symbols on paper

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_number - pfunction that prints an integer.
+ * @n: number to print
+ * Return: print _putchar
+ */
+
+void print_number(int n)
+{
+	int nprint;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		nprint = -n;
+	}
+	else
+	{
+		nprint = n;
+	}
+	if (nprint / 10)
+	{
+		print_number(nprint / 10);
+	}
+	_putchar(nprint % 10 + '0');
+}
