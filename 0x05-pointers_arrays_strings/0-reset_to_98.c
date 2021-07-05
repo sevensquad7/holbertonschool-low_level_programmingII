343026116251ffb41964ef33ebfb27d89c8cd910
zye7ert <edgarxot@gmail.com>
2021-07-05 01:32:17 +0000

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter 
+ * reset_to_98 - function that takes a pointer to an int as parameter
  * and updates the value it points to to 98.
  * @n: integer to change
  * Return: void
