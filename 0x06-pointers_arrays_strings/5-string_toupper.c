665e85ed87bfdd16b580f00ab85ac59b1fde2ec4
zye7ert <edgarxot@gmail.com>
2021-07-16 06:10:00 +0000

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters
+ * of a string to uppercase.
+ * @s: string to change
+ *
+ * Return: address of s
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
+}
