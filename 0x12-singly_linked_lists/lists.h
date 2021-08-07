fac98f6dc1acf56358172d499b8272abf9d195df
zye7ert <edgarxot@gmail.com>
2021-08-06 23:15:57 -0500

    upload lists.h in the proyect

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,12 +5,6 @@
 #include <string.h>
 #include <stddef.h>
 
-size_t print_list(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-size_t list_len(const list_t *h);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -25,6 +19,13 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+int _putchar(char);
+size_t print_list(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /*HOLBERTON_H*/
 
