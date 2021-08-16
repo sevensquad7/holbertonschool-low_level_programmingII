f7ccddf44290228fc54c14fd1d9a3c241fc8850a
zye7ert <edgarxot@gmail.com>
2021-08-15 19:32:54 -0500

    update files, added line in the files more singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_nodeint - function that adds a new node at the beginning 
+ * add_nodeint - function that adds a new node at the beginning
  * of a listint_t lists
  * @head: a list of elements with a pointer to the first element
  * @n: a element of the node
@@ -10,6 +10,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
