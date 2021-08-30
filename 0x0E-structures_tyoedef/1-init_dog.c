29d653dec801b216de728a857400d627ab20d2a2
zye7ert <edgarxot@gmail.com>
2021-08-29 21:34:11 -0500

    C - Structures, typedef

---
+++ 0x0E-structures_tyoedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - function
+ * @d: data structed
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+}
---
+++ 0x0E-structures_tyoedef/README.md
@@ -0,0 +1,5 @@
+# Structures
+C programming
+A structure is a user defined data type available
+in C that allows to combine data items of
+different kinds.
