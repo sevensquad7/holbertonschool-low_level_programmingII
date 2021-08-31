e2aca793afdaf4d3ffae63a5a48a1a00e030e7f6
zye7ert <edgarxot@gmail.com>
2021-08-31 00:05:16 -0500

    added library in the main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,6 @@
 
-#ifndef HOLBERTON_IO
-#define HOLBERTON_IO
+#ifndef _HOLBERTON_IO_
+#define _HOLBERTON_IO_
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -14,4 +14,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* HOLBERTON_IO */
+#endif /* _HOLBERTON_IO_ */
