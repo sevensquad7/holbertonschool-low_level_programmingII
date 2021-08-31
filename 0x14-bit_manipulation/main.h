64acde18e9aaa4824615a076cdde23962d06ea9c
zye7ert <edgarxot@gmail.com>
2021-08-31 02:40:16 -0500

    added task project C - Bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,13 +7,12 @@
 #include <stdint.h>
 
 int _putchar(char c);
-unsigned int _pow(unsigned int base, unsigned int power);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int main(void);
+int get_endianness(void);
 
 #endif /* HOLBERTON_BIT */
