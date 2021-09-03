8cfdf247bc564be4d8f5fc6816b76923d57dd24f
zye7ert <edgarxot@gmail.com>
2021-09-03 18:00:59 -0500

    update task

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,37 +1,6 @@
 #include "main.h"
 
 /**
- * _pow - returns the power raised to the base number
- * @base: base number
- * @power: times to multiply the base
- *
- * Return: pow
- */
-
-unsigned int _pow(unsigned int base, unsigned int power)
-{
-	unsigned int i;
-	unsigned int pow = 1;
-
-	if (power == 0)
-	{
-		return (1);
-	}
-	else if (power == 1)
-	{
-		return (base);
-	}
-	else
-	{
-		for (i = 0; i < power; i++)
-		{
-			pow *= base;
-		}
-		return (pow);
-	}
-}
-
-/**
  * clear_bit - sets the value of a bit to 0 at a given index
  * @n: decimal number
  * @index: the index, starting from 0 of the bit you want to set
@@ -41,21 +10,10 @@ unsigned int _pow(unsigned int base, unsigned int power)
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > (sizeof(unsigned long int) * 8))
-	{
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	}
-	else if (*n == 0)
-	{
-		*n = 0;
-	}
-	else if (_pow(2, index) > *n)
-	{
-		return (1);
-	}
-	else
-	{
-		*n = *n - _pow(2, index);
-	}
+
+	*n &= ~(1 << index);
+
 	return (1);
 }
