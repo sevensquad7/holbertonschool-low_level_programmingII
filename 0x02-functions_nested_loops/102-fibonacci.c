3a63e80812f621556518f03fa3434aee2a35698a
zye7ert <edgarxot@gmail.com>
2021-09-02 21:04:22 -0500

    update task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
-/**	
+/**
  * main - Write a program that prints the first 50 Fibonacci numbers,
  * starting with 1 and 2, followed by a new line.
- *	
- * Return: On success 1.	
- * On error, -1 is returned, and errno is set appropriately.	
- */	
-int main(void)	
-{	
-	int i;	
-	long t1 = 0, t2 = 1, nextTerm;	
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+int i;
+long t1 = 0, t2 = 1, nextTerm;
 
-	for (i = 1; i <= 50; ++i)	
-	{	
-		nextTerm = t1 + t2;	
-		if (i == 50)	
-			printf("%ld", nextTerm);	
-		else	
-			printf("%ld, ", nextTerm);	
-		t1 = t2;	
-		t2 = nextTerm;	
-	}	
-	printf("\n");	
-	return (0);	
+for (i = 1; i <= 50; ++i)
+{
+nextTerm = t1 + t2;
+if (i == 50)
+printf("%ld", nextTerm);
+else
+printf("%ld, ", nextTerm);
+t1 = t2;
+t2 = nextTerm;
+}
+printf("\n");
+return (0);
 }
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-/**	
+/**
  * main - Write a program that finds and prints the first 98 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line.	
- *	
- * Return: Always 0.	
- */	
-int main(void)	
-{	
-	return (0);	
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+return (0);
 }
