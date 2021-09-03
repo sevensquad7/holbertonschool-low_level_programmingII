69a962cbf4a6b08ed07f5de411cab9d7a6a11b97
zye7ert <edgarxot@gmail.com>
2021-09-02 20:53:48 -0500

    update task

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**	
+ * main - Write a program that finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.	
+ *	
+ * Return: Always 0.	
+ */	
+int main(void)	
+{	
+	return (0);	
+}
