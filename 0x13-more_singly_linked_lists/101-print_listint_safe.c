bd9160ea52c8819d8f29e580cf3225906a5ee8d3
zye7ert <edgarxot@gmail.com>
2021-09-03 11:46:46 -0500

    update task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,48 +1,77 @@
 #include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
 
+/**
+* add_node - function that adds a new node at the start of adrsList list
+* @head: pointer to pointer to adrsList list
+* @ptr: address to be added
+* Return: the address of head, or NULL if it failed
+**/
+adrsList *add_node(adrsList **head, void *ptr)
+{
+	adrsList *new;
+
+	new = malloc(sizeof(adrsList));
+	if (new == NULL)
+	{
+		free_adrsList(*head);
+		exit(98);
+	}
+	(*new).ptr = ptr;
+	(*new).next = *head;
+	*head = new;
+	return (*head);
+}
 
 /**
-* print_listint_safe - print linked list even if its circular
-* @head: pouinter to the heder
-*
-* Return: nb of nodes
-*/
+* free_adrsList - frees newly craeted address list
+* @h: head of the list
+**/
+void free_adrsList(adrsList *h)
+{
+	adrsList *frN;
 
+	while (h)
+	{
+		frN = h;
+		h = (*h).next;
+		free(frN);
+	}
+}
+
+/**
+* print_listint_safe - prints listint_t list, safe version
+* @head: pointer to elements of type listint_t
+* Return: the number of nodes in the list
+**/
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current, *ptr;
-	int found = 0;
-	const listint_t *array[50];
-	size_t i, n = 0;
+	size_t counter;
+	adrsList *newList, *newHead;
 
-	if (!head)
-		return (0);
-	current = ptr = head;
-	n = 0;
-	while (current)
+	counter = 0;
+	if (head == NULL)
+		return (counter);
+
+	newHead = newList = NULL;
+	while (head)
 	{
-		ptr = current;
-		for (i = 0; i < n; i++)
+		while (newList)
 		{
-			if (array[i] == current)
+			if ((*newList).ptr == head)
 			{
-				found = 1;
-				break;
+				printf("-> [%p] %d\n", (void *)head, (*head).n);
+				free_adrsList(newHead);
+				return (counter);
 			}
+			newList = (*newList).next;
 		}
-		if (found)
-		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			free((void *)ptr);
-			return (n);
-		}
-		array[n] = current;
-		printf("[%p] %d\n", (void *)current, current->n);
-		free((void *)ptr);
-		current = current->next;
-		n++;
+		printf("[%p] %d\n", (void *)head, (*head).n);
+		newList = add_node(&newHead, (void *)head);
+		head = (*head).next;
+		counter++;
 	}
-	free((void *)current);
-	return (n);
+	free_adrsList(newHead);
+	return (counter);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,31 +1,42 @@
 #include "lists.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* free_listint_safe - free list with a loop
-* @h: header pointer
-*
-* Return: nb node
-*/
-
+* free_listint_safe - function that frees linked list
+* @h: pointer to pointer to elements of type listint_t
+* Return: the number of nodes in the list that were freed
+**/
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *prev = NULL;
-	listint_t *current = NULL;
-	size_t count = 0;
+	size_t counter;
+	listint_t *temp;
+	adrsList *newList, *newHead;
 
-	if (!h)
-		return (0);
-
-	current = *h;
-	while (current)
+	counter = 0;
+	if (*h == NULL)
+		return (counter);
+	newHead = newList = NULL;
+	while (*h)
 	{
-		prev = current;
-		current = current->next;
-		free(prev);
-		count++;
+		while (newList)
+		{
+			if ((*newList).ptr == *h)
+			{
+				free_adrsList(newHead);
+				newHead = NULL;
+				*h = NULL;
+				return (counter);
+			}
+			newList = (*newList).next;
+		}
+		newList = add_node(&newHead, (void *)*h);
+		temp = *h;
+		*h = (*(*h)).next;
+		free(temp);
+		counter++;
 	}
-
+	free_adrsList(newHead);
 	*h = NULL;
-	return (count);
+	return (counter);
 }
