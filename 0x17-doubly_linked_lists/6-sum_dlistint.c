06de25721ca3931b053d2a4d6b212a6196330307
zye7ert <edgarxot@gmail.com>
2021-09-19 23:15:08 -0500

    Sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Return sum
+ * @head: List
+ *
+ * Return: Sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
